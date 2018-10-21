975fa0a271eb451135302e2fdfee9fc1b3e16c83
Angelo Bazelais <a.bazelais.25@theacademychi.org>
2018-10-21 18:39:00 +0000

    edit root

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get 'todo/edit/:id', to: 'todo#edit'
   get 'todo/update/:id', to: 'todo#update'
   get 'todo/destroy/:id', to: 'todo#destroy'
+  root to: 'todo#index'
 end

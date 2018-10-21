82a342113f9ab94b01ab8340a1e5a1f3afcfa96e
Angelo Bazelais <a.bazelais.25@theacademychi.org>
2018-10-21 16:45:04 +0000

    add todo form

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,10 +1,20 @@
 class TodoController < ApplicationController
     def index 
         @todos= Todo.all
-        
+        @todo = Todo.new
+    
     end
     
     def show
         @todo = Todo.find_by_id(params[:id])
+    end
+    def new
+    end
+    def create
+        t =Todo.new
+        t.description = params['description']
+        t.pomodoro_estimate =  params['pomodoro_estimate']
+        t.save
+        redirect_to "/todo/show/#{ t.id }"
     end
 end
